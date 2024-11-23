109708ec5acf0d528c454b86d1393844becefe23
Paretoo <110358152+Paretoo@users.noreply.github.com>
2024-11-23 18:20:21 +0100

    Update snooker.js
    Bug fix

--- snooker.js
+++ snooker.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     document.getElementById("box1").value = points;
-    document.getElementById("box2").value = points;
+    document.getElementById("box2").value = points2;
     
 };
 
@@ -48,35 +48,35 @@ function score7() {
 
 document.getElementById("red2").addEventListener("click", score8);
 function score8() {
-    document.getElementById("box2").value = points+=1;
+    document.getElementById("box2").value = points2+=1;
 };
 
 document.getElementById("yellow2").addEventListener("click", score9);
 function score9() {
-    document.getElementById("box2").value = points+=2;
+    document.getElementById("box2").value = points2+=2;
 };
 
 document.getElementById("green2").addEventListener("click", score10);
 function score10() {
-    document.getElementById("box2").value = points+=3;
+    document.getElementById("box2").value = points2+=3;
 };
 
 document.getElementById("brown2").addEventListener("click", score11);
 function score11() {
-    document.getElementById("box2").value = points+=4;
+    document.getElementById("box2").value = points2+=4;
 };
 
 document.getElementById("blue2").addEventListener("click", score12);
 function score12() {
-    document.getElementById("box2").value = points+=5;
+    document.getElementById("box2").value = points2+=5;
 };
 
 document.getElementById("pink2").addEventListener("click", score13);
 function score13() {
-    document.getElementById("box2").value = points+=6;
+    document.getElementById("box2").value = points2+=6;
 };
 
 document.getElementById("black2").addEventListener("click", score14);
 function score14() {
-    document.getElementById("box2").value = points+=7;
+    document.getElementById("box2").value = points2+=7;
 };
